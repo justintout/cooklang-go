3faa3e12f2060879d27d37d0cafca46f467b8c87
Justin Tout <3639630+justintout@users.noreply.github.com>
2022-05-04 19:46:25 -0400

    parse canonical test yaml to table-based tests, test metadata

--- canonical_test.go
+++ canonical_test.go
@@ -1,7 +1,6 @@
 package cooklang_test
 
 import (
-	"fmt"
 	"os"
 	"testing"
 
@@ -9,41 +8,54 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-// https://raw.githubusercontent.com/cooklang/spec/main/tests/canonical.yaml
-// version: 5
-
-type CanonicalTests struct {
-	Version string
-	Tests   map[string]struct {
-		Source string
-		Result struct {
-			Steps    [][]cooklang.CanonicalDirectionItem
-			Metadata map[string]interface{}
+type canonicalTest struct {
+	Source string
+	Result struct {
+		Metadata map[string]string
+		Steps    [][]struct {
+			Type     string
+			Name     string
+			Quantity string
+			Units    string
+			Value    string
 		}
 	}
 }
 
-func TestCanonical(t *testing.T) {
-	t.Skip("this doesn't work yet.")
-	cy, err := os.ReadFile("canonical.yaml")
-	if err != nil {
-		t.Fatalf("failed to read canonical.yaml: %v", err)
-	}
-	var ct CanonicalTests
-	if err := yaml.Unmarshal([]byte(cy), &ct); err != nil {
-		t.Fatalf("failed to unmarshal canonical.yaml: %v", err)
-	}
+type canonicalTests struct {
+	Version string
+	Tests   map[string]canonicalTest
+}
 
-	for i, tt := range ct.Tests {
-		fmt.Println(tt.Source)
-		r, err := cooklang.Parse(tt.Source)
-		if err != nil {
-			t.Errorf("error parsing canonical test %q: %v", i, err)
-		}
-		y, err := yaml.Marshal(&r)
-		fmt.Printf("- %s\n", tt.Source)
-		w, _ := yaml.Marshal(tt.Result)
-		fmt.Printf("%s\n", string(y))
-		fmt.Printf("\twant: %s\n", string(w))
+func TestCanonical(t *testing.T) {
+	b, err := os.ReadFile("canonical.yaml")
+	if err != nil {
+		panic("failed to read canonical.yaml: " + err.Error())
+	}
+	var tests canonicalTests
+	err = yaml.Unmarshal(b, &tests)
+	if err != nil {
+		panic("failed to unmarshal tests: " + err.Error())
+	}
+	t.Logf("canonical tests version %s\n", tests.Version)
+	for name, test := range tests.Tests {
+		t.Run(name, func(t *testing.T) {
+			r := cooklang.MustParse(test.Source)
+
+			for tk, tv := range test.Result.Metadata {
+				found := false
+				for k, v := range *r.Metadata {
+					if k == tk {
+						found = true
+						if v != tv {
+							t.Errorf("%q value incorrect, got: %v:%v, want: %v:%v", tk, k, v, tk, tv)
+						}
+					}
+				}
+				if !found {
+					t.Errorf("did not find metadata key %q, got: %v, want: %v", tk, r.Metadata, test.Result.Metadata)
+				}
+			}
+		})
 	}
 }
